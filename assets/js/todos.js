1f28033389bb1ae8d706e011846e0e6ae9a0aa87
Arthur Anastopulos <arthuranastopulos@hotmail.com>
2020-10-11 16:06:52 -0300

    Delete Todos function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,10 @@
 $("li").click(function () { 
     $(this).toggleClass("completed");
 });
+
+$("span").click(function(Event){
+    $(this).parent().fadeOut("slow", function() {
+        $(this).remove();
+    });
+    Event.stopPropagation();
+});
