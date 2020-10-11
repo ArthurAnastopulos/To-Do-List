6c21de9217dd728cd17243baa3f267921f0c151d
Arthur Anastopulos <arthuranastopulos@hotmail.com>
2020-10-11 16:21:08 -0300

    Add new elements fot

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,18 @@
-$("li").click(function () { 
+$("ul").on("click", "li", function () { 
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(Event){
+$("ul").on("click", "span",function(Event){
     $(this).parent().fadeOut("slow", function() {
         $(this).remove();
     });
     Event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(Event){
+    if(Event.which === 13) {
+        var input = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + input + "</li>");
+    }
 });
